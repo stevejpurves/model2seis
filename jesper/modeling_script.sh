fec92b5df77126d6befa6b6ea3b298c057f345e8
Jesper Dramsch <jesper.dramsch@zmaw.de>
2017-06-05 16:52:57 +0200

    Timing and waiting for jobs

--- jesper/modeling_script.sh
+++ jesper/modeling_script.sh
@@ -1,29 +1,33 @@
 #!/bin/bash
-
 #This script models shots for the Marmousi data set using sufdmod2.
 #It has to be looped becuase the algorithm only models one shot
 #at a time. 
 
+STARTTIME=$(date +%s)
 fold=/mnt/y/Hackathon/model2seis/jesper
+
 delx=25
 delz=25
-numShot=50
+numShot=30
 dShot=2
 out=output
 
-rm $fold/$out
+rm $fold/$out*
 
 foo () {
 	shotx=$(($1*$dShot*$delx))
 	shotz=$((1*$delz))
 	echo -e "\nShot number $index1 at x location: $shotx meters"
-	sufdmod2 <$fold/test.bin >$fold/temp$1.su nx=500 nz=250 xs=$shotx zs=$shotz \
-		 tmax=4 hsz=15 hsfile=$fold/$1.su abs=1,1,1,1 verbose=1 dx=$delx \
+	sufdmod2 <$fold/test.bin > /dev/null nx=500 nz=250 xs=$shotx zs=$shotz \
+		 tmax=4 hsz=15 hsfile=$fold/hsfile$1.su abs=0,1,1,1 verbose=0 dx=$delx \
 		 dz=$delz fmax=150
-	sushw < $fold/$1.su a=$1 b=0 c=1 j=500 key=ep >> $fold/$out$1
+	sushw < $fold/hsfile$1.su a=$1 key=ep >> $fold/$out$1
+	rm hsfile$1.su
 }
 
-for index1 in $(seq 1 $numShot); do foo "$index1" & done
+for index1 in $(seq 1 $numShot);do foo "$index1" & done
+
+wait $(jobs -p)
 
 echo "Preparing output."
 fcat $fold/$out* > $fold/$out
@@ -31,5 +35,7 @@ susort ep offset < $fold/$out |
 sushw key=tracl a=1 b=1 > $fold/shots.su
 
 rm $fold/$out*
-rm $fold/temp*.su
+
+ENDTIME=$(date +%s)
+echo "It takes $(($ENDTIME - $STARTTIME)) seconds to complete this task..."
 
