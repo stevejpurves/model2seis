4ea1a59c72f5b123424c3564a1969c46037f9c1e
gram <gr@m-gan.sl>
2017-06-06 10:31:54 -0500

    cleaning up migration geoms

--- fdmod/modeling_script.sh
+++ fdmod/modeling_script.sh
@@ -4,7 +4,7 @@
 #It has to be looped becuase the algorithm only models one shot
 #at a time. 
 
-fold=.
+fold=../dat
 delx=25
 delz=25
 numShot=50
@@ -18,9 +18,9 @@ for (( index1=1; index1<=$numShot; index1++ ))
 	shotx=$(($index1*$dShot*$delx))
 	shotz=$((1*$delz))
 	echo -e "\nShot number $index1 at x location: $shotx meters"
-	sufdmod2 <$fold/mod.bin >$fold/temp.su nx=50 nz=100 xs=$shotx zs=$shotz \
-		 tmax=4 hsz=15 hsfile=$fold/$index1.su abs=0,1,1,1 verbose=1 dx=$delx \
-		 dz=$delz fmax=150
+	sufdmod2 <$fold/simple.bin >$fold/temp.su nx=500 nz=250 xs=$shotx zs=$shotz \
+		 tmax=2 hsz=15 hsfile=$fold/$index1.su abs=0,1,1,1 verbose=1 dx=$delx \
+		 dz=$delz fmax=250
 	cat $fold/$index1.su >> $fold/$out
 	rm $fold/$index1.su
 	done
--- migration/kirchhoff_migScript.sh
+++ migration/kirchhoff_migScript.sh
@@ -4,13 +4,11 @@ fold=../dat
 data=shots.su
 mig=mig_kirchhoff.su
 tt=travTime.bin
-vel=mod.bin
+vel=simple.bin
 
-rayt2d vfile=$fold/$vel tfile=$fold/$tt dt=.004 nt=1000 fz=0 nz=100 dz=25 \
-       fx=0 nx=50 dx=25 fzo=0 nzo=100 dz=25 fxo=0 nxo=50 dxo=25 fxs=0 \
-       nxs=50 ms=10
+rayt2d vfile=$fold/$vel tfile=$fold/$tt dt=.004 nt=1000 fz=0 nz=250 dz=25 \
+       fx=0 nx=500 dx=25 fxs=0 nxs=500 dxs=25 ms=100
 
 sukdmig2d infile=$fold/$data outfile=$fold/$mig ttfile=$fold/$tt \
-          fzt=0 nzt=100 dzt=25 fxt=0 nxt=50 dxt=25 fs=0 ns=50 \
-          ds=25 dxm=12.5 dzo=5 nzo=605 dxo=25 nxo=50 doff=25      \
-          noff=200 fmax=150
+          fzt=0 nzt=250 dzt=25 fxt=0 nxt=500 dxt=25 fs=0 ns=500 \
+          ds=25 dzo=5 nzo=605 dxo=25 nxo=500
